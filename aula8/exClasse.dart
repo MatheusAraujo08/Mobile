55bb8f6ec3e093c5132bc8a07bcccda0f97e27f1
RyanP0612 <128430984+RyanP0612@users.noreply.github.com>
2023-09-13 20:08:44 -0300

    Criar função principal
    Criação da função principal

--- aula8/exClasse.dart
+++ aula8/exClasse.dart
@@ -2,3 +2,10 @@
 class Casa {
   String? cor; // atributo cor
 }
+
+void main() {
+  // instancia a classe com o objeto casa
+  Casa minhaCasa = Casa();
+  minhaCasa.cor = "Blue";
+  print(minhaCasa.cor);
+}
