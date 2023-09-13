7c20eef3ee0dbc7c91bc1f7a1ca890f17b1ade08
RyanP0612 <128430984+RyanP0612@users.noreply.github.com>
2023-09-13 20:45:49 -0300

    Exemplo de criação de calsses

--- aula8/exClasse.dart
+++ aula8/exClasse.dart
@@ -1,6 +1,20 @@
 // criar classe chamada casa
 class Casa {
   String? cor; // atributo cor
+
+// criar metodos
+  void abrirJanelas(int qtde_janelas) {
+    print("Abrir janela, qtde de janelas $qtde_janelas");
+  }
+
+  void abrirPorta() {
+    print("Porta aberta");
+  }
+
+  void abrirCasa() {
+    this.abrirJanelas(2);
+    this.abrirPorta();
+  }
 }
 
 void main() {
@@ -8,4 +22,13 @@ void main() {
   Casa minhaCasa = Casa();
   minhaCasa.cor = "Blue";
   print(minhaCasa.cor);
+  minhaCasa.abrirPorta();
+  minhaCasa.abrirJanelas(3);
+  minhaCasa.abrirCasa();
+
+  Casa imovel = Casa();
+  imovel.cor = "Red";
+  print(imovel.cor);
+  imovel.valor = 500.000;
+  print("O valor do imovel é de R$  ${imovel.valor}");
 }
